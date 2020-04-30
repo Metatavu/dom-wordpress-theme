09c20fa958a43f5ace7f902270accbc2a171f68e
Daniil Smirnov <daniilsmirnov@Daniils-MacBook-Pro-2.local>
2020-04-30 11:43:58 +0300

    Url fixed + removed menus

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 
 					<div class="image-container">
 						<img class="header-image" src="<?php echo get_stylesheet_directory_uri(); ?>/logo/header_background.png" alt="header background" />
-						<a href="http://localhost/index.php/">
+						<a href="<?php echo home_url(); ?>">
 							<img class="header-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/logo/dom_white.png" alt="bar dom logo" />
 						</a>
 						<p class="header-text-line1">TAKEAWAY</p>
@@ -51,25 +51,6 @@
 							<p class="text-area-header">Tilaa täältä ja nouda DOMista</p>
 							<p class="text-area-content">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 						</div><!-- .text-area -->
-					<?php endif; ?>
-
-					<?php if ( is_home() && is_front_page() ) : ?>
-						<body>
-							<div id="site-menu" class="site-menu">
-								<a href="http://localhost/index.php/product-category/beers/">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/logo/beer.png" alt="beer image" style="width:300px;height:302px;padding:10px;">
-									<p class="menu-item" style="top:45%;left:24%;">Oluet</p> <!-- Those imbedded styles are for demonstration purpose only, they break upon page resizing -->
-								</a>
-								<a href="http://localhost/index.php/product-category/beers/">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/logo/cider.png" alt="beer image" style="width:300px;height:302px;padding:10px;">
-									<p class="menu-item" style="top:45%;left:46%;">Siiderit</p> <!-- Those imbedded styles are for demonstration purpose only, they break upon page resizing -->
-								</a>
-								<a href="http://localhost/index.php/product-category/beers/">
-									<img src="<?php echo get_stylesheet_directory_uri(); ?>/logo/long.png" alt="beer image" style="width:300px;height:302px;padding:10px;">
-									<p class="menu-item" style="top:45%;left:67%;">Long drinkit</p> <!-- Those imbedded styles are for demonstration purpose only, they break upon page resizing -->
-								</a>
-							</div><!-- .site-header-menu -->
-						</body>
 					<?php endif; ?>
 
 		</header><!-- #site-header -->
